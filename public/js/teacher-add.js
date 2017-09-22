117f95e31aa6e2cc495b74bf19152d3edf57bb50
pingmeiniu <917749518@qq.com>
2017-09-22 16:56:44 +0800

    添加和编辑讲师的验证功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,7 +1,7 @@
 /**
  * Created by acer on 2017/9/21.
  */
-define(['jquery','template','util','datepicker','language'],function ($,template,util) {
+define(['jquery','template','util','datepicker','language','validate','form'],function ($,template,util) {
    var tcId = util.qs('tc_id');
     if(tcId){
         $.ajax({
@@ -22,20 +22,34 @@ define(['jquery','template','util','datepicker','language'],function ($,template
     }
 
     function submitForm(url){
-        $("#teacherBtn").click(function () {
-            $.ajax({
-                url:url,
-                type:"post",
-                dataType:"json",
-                data:$("#teacherForm").serialize(),
-                success: function (data) {
-                    //console.log(data);
-                    if(data.code==200){
-                        location.href="/teacher/teacher_list";
-                    }
+        $("#teacherForm").validate({
+            sendForm:false,
+            valid: function () {
+               $(this).ajaxSubmit({
+                   url:url,
+                   type:"post",
+                   dataType:"json",
+                   //data:$("#teacherForm").serialize(),
+                   success: function (data) {
+                       //console.log(data);
+                       if(data.code==200){
+                           location.href="/teacher/teacher_list";
+                       }
+                   }
+               });
+            },
+            description:{
+                tcName:{
+                    required:'用户名不能为空'
+                },
+                tcPass:{
+                    required:'密码不能为空',
+                    pattern:'密码必须是6位数字'
+                },
+                tcJoinDate:{
+                    required:'日期不能为空'
                 }
-            });
+            }
         });
-
     }
 });
